81b7fee3eb8a06c4937620cb8eb6f33bd459929b
gnasah <getup1214@gmail.com>
2023-11-18 20:39:04 +0900

    base html, css and hits

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// const currentLocation = location.href;
+// const navItems = document.querySelectorAll("nav a");
+
+// navItems.forEach(item => {
+//     if(item.href === currentLocation){
+//         item.classList.add("active");
+//     }
+// });
